e08bb9543fdf066be3582769877d994344558b72
mmislam <mmislam272@gmail.com>
2024-09-06 20:31:25 +0600

    backend server setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+// routers ...
+
+app.use('/api/users', userRouter)
+
+
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => { console.log(`Serving at localhost:${port}`) })
