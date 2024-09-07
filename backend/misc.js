ac4056e7a4fe5953410506e6c108a90bf78772a9
mmislam <mmislam272@gmail.com>
2024-09-07 18:03:35 +0600

    user router

--- backend/misc.js
+++ backend/misc.js
@@ -1,49 +1,41 @@
-import express from 'express'
-import User from '../models/userModel.js'
-import expressAsyncHandler from 'express-async-handler';
-import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import mg from 'mailgun-js';
 
+export const generateToken = (user) => {
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      isSeller: user.isSeller,
+    },
+    process.env.JWT_SECRET || 'somethingsecret',
+    {
+      expiresIn: '30d',
+    }
+  );
+};
 
-const userRouter = express.Router()
-
-
-userRouter.post(
-    '/register',
-    expressAsyncHandler(async (req, res) => {
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: bcrypt.hashSync(req.body.password, 8),
-            
-        });
-        const newUser = await user.save();
-        res.send({
-            _id: newUser._id,
-            name: newUser.name,
-            email: newUser.email,
-            token: generateToken(newUser),
-        });
-    })
-);
-
-userRouter.post(
-    '/signin',
-    expressAsyncHandler(async (req, res) => {
-        const user = await User.findOne({ email: req.body.email });
-        if (user) {
-            if (bcrypt.compareSync(req.body.password, user.password)) {
-                res.send({
-                    _id: user._id,
-                    name: user.name,
-                    email: user.email,
-                    token: generateToken(user),
-                });
-                return;
-            }
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || 'somethingsecret',
+      (err, decode) => {
+        if (err) {
+          res.status(401).send({ message: 'Invalid Token' });
+        } else {
+          req.user = decode;
+          next();
         }
-        res.status(401).send({ message: 'Invalid email or password' });
-    })
-);
+      }
+    );
+  } else {
+    res.status(401).send({ message: 'No Token' });
+  }
+};
 
 
-export default userRouter
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,52 +1,76 @@
-
-import express from 'express'
-import User from '../models/userModel.js'
+import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import bcrypt from 'bcryptjs';
-import { generateToken } from '../misc.js';
+import User from '../models/userModel.js';
+import { generateToken,  isAuth } from '../misc.js';
 
-const userRouter = express.Router()
+const userRouter = express.Router();
+
 
 
 userRouter.post(
-    '/register',
-    expressAsyncHandler(async (req, res) => {
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: bcrypt.hashSync(req.body.password, 8),
-            
-        });
-        const newUser = await user.save();
+  '/signin',
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
         res.send({
-            _id: newUser._id,
-            name: newUser.name,
-            email: newUser.email,
-            
-            token: generateToken(newUser),
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          
+          token: generateToken(user),
         });
-    })
+        return;
+      }
+    }
+    res.status(401).send({ message: 'Invalid email or password' });
+  })
 );
 
 userRouter.post(
-    '/signin',
-    expressAsyncHandler(async (req, res) => {
-        const user = await User.findOne({ email: req.body.email });
-        if (user) {
-            if (bcrypt.compareSync(req.body.password, user.password)) {
-                res.send({
-                    _id: user._id,
-                    name: user.name,
-                    email: user.email,
-                    
-                    token: generateToken(user),
-                });
-                return;
-            }
-        }
-        res.status(401).send({ message: 'Invalid email or password' });
-    })
+  '/register',
+  expressAsyncHandler(async (req, res) => {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8),
+    });
+    const createdUser = await user.save();
+    res.send({
+      _id: createdUser._id,
+      name: createdUser.name,
+      email: createdUser.email,
+      
+      token: generateToken(createdUser),
+    });
+  })
+);
+
+userRouter.get(
+  '/:id',
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(404).send({ message: 'User Not Found' });
+    }
+  })
 );
 
 
-export default userRouter
+userRouter.get(
+  '/',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.send(users);
+  })
+);
+
+
+
+
+export default userRouter;
