77436108cf3cfc613d89d798970caece0d99e072
mmislam <mmislam272@gmail.com>
2024-09-07 16:23:06 +0600

    database setup

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,23 @@
 import express from "express";
+import mongoose from 'mongoose'
 import dotenv from 'dotenv';
+import userRouter from './routers/userRouter.js'
 
 
 dotenv.config();
 const app = express()
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const conn = process.env.DATABASE_URL
 
+mongoose.connect(conn)
 
 
 // routers ...
 
 app.use('/api/users', userRouter)
+
 
 
 
